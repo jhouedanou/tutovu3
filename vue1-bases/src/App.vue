c9ab2fbd47bf59d3c568afafe43867683628b8c9
Jean Luc Houédanou <jhouedanou@gmail.com>
2024-12-30 10:59:32 +0000

    f

--- vue1-bases/src/App.vue
+++ vue1-bases/src/App.vue
@@ -1,19 +1,36 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from "vue";
+import "bulma/css/bulma.min.css";
+
 //var
-const items = ref(['Item 1', 'Item 2', 'Item 3']);
+let id = 0;
+const msg = ref("Tuto VUE 3");
+const fName = ref("Jean-Luc Lionel");
+const lName = ref("Houédanou");
 //fn
-
-
+const fullName = computed(() => {
+  return fName.value + " " + lName.value.toUpperCase();
+});
 </script>
-
 <template>
-  <main>
-<h2>{{msg}}</h2>
-<ul><li>Item i</li></ul>
-  </main>
+  <div class="page-wrapper">
+    <main class="container">
+      <h2 class="title is-2">{{ msg }}</h2>
+      <p class="subtitle">Bonjour, {{ fullName }}</p>
+    </main>
+  </div>
 </template>
+
 <style lang="scss">
+.page-wrapper {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-
+.container {
+  width: 60vw;
+  margin: 0 auto;
+}
 </style>
---
+++ vue1-bases/src/vFor.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref } from "vue";
+//var
+let id = 0;
+const msg = ref("Tuto VUE 3");
+let newItem = ref("");
+const items = ref([
+  { id: id++, text: "Item 1" },
+  { id: id++, text: "Item 2" },
+  { id: id++, text: "Item 3" },
+]);
+//fn
+const addItem = () => {
+  items.value.push({ id: id++, text: newItem.value });
+  newItem.value = "";
+};
+const removeItem = (id) => {
+  items.value = items.value.filter((item) => item !== id);
+};
+</script>
+
+<template>
+  <div class="page-wrapper">
+    <main>
+      <h2>{{ msg }}</h2>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          {{ item.text }}
+          <button class="button is-primary is-medium" @click="removeItem(item)">
+            x
+          </button>
+        </li>
+      </ul>
+      <div class="control">
+        <input type="text" v-model="newItem" />
+      </div>
+
+      <div
+        class="is-flex is-align-items-center is-justify-content-space-between"
+      >
+        <button class="button is-primary is-medium" @click="addItem">
+          Add Item
+        </button>
+      </div>
+    </main>
+  </div>
+</template>
+<style lang="scss">
+.page-wrapper {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  width: 60vw;
+  margin: 0 auto;
+}
+</style>
