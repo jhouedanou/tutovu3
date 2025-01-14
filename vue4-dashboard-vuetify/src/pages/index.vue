095d97a93052a74013cc5c654785676ebeeb89bf
Jean Luc Houedanou <jeanluc@houedanou.com>
2025-01-14 16:36:22 +0000

    w

---
+++ vue4-dashboard-vuetify/src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-app-bar
+      class="px-3"
+      color="grey-darken-4"
+      flat
+      density="compact"
+    >
+      <v-tabs
+        v-model="selectedTab"
+        centered
+        color="grey-darken-1"
+      />
+    </v-app-bar>
+  </v-app>
+</template>
+<script lang="ts" setup>
+//imports
+import{ref,watch , onMounted} from 'vue';
+let selectedTab = ref(0);
+const links = ref(["Dashboard","About"]);
+</script>
