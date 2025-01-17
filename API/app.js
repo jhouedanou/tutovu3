04f6ab4cd74efd9ab630e56c5302836f044ca4d5
Jean Luc Houedanou <jeanluc@houedanou.com>
2025-01-17 15:06:45 +0000

    api

--- API/app.js
+++ API/app.js
@@ -1,1 +1,14 @@
-console.log('API');
+import {createServer} from 'node:http'
+
+createServer(async(req,res)=>{
+    res.setHeader('Content-Type','application/json')
+    //ajouter les headers pour les access control allow origin
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,Accept,Origin, Auhorization')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,PATCH,OPTIONS')
+
+    const url  = new URL(req.url,`http://${req.headers.host}`)
+
+
+    console.log('url:',url)
+}).listen(3002)
